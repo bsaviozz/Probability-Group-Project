40612272255e073ec9a56954b84362fe15a898a0
bsaviozz <127849420+bsaviozz@users.noreply.github.com>
2023-12-04 20:35:38 +0000

    Update question3_s1.R

--- question3_s1.R
+++ question3_s1.R
@@ -1,9 +1,9 @@
 library(ggplot2)
-game <- function(x, n, max){
+game <- function(x, n){
   win <- 0
   timesToRun <- 10000
   numSkips <- n / x
-  
+  max <- n*10
   j <- 0
   while (j < timesToRun)
   {
@@ -43,10 +43,10 @@ game <- function(x, n, max){
   return(p)
 }
 
-count <- seq(2,25, by=0.5)
+count <- seq(2,15, by=0.3)
 probabilities <- c(0)
 for(i in count){
-  probabilities <- c(probabilities, game(i, 100, 1000))
+  probabilities <- c(probabilities, game(i, 100))
 }
 count <- c(0, count)
 probabilities <- data.frame(x = count, p = probabilities)
@@ -56,10 +56,10 @@ ggplot(probabilities, aes(x=x, y=p)) +
   scale_x_continuous(labels = as.character(count), breaks = count)
 
 
-count <- seq(2,25, by=0.5)
+count <- seq(2,15, by=0.3)
 probabilities <- c(0)
 for(i in count){
-  probabilities <- c(probabilities, game(i, 1000, 10000))
+  probabilities <- c(probabilities, game(i, 1000))
 }
 count <- c(0, count)
 probabilities <- data.frame(x = count, p = probabilities)
@@ -67,10 +67,10 @@ ggplot(probabilities, aes(x=x, y=p)) +
   geom_line( color="#3632a8", linewidth=1, alpha=0.9, linetype="solid") 
 
 
-count <- seq(2,25, by=0.5)
+count <- seq(2,15, by=0.3)
 probabilities <- c(0)
 for(i in count){
-  probabilities <- c(probabilities, game(i, 10000, 100000))
+  probabilities <- c(probabilities, game(i, 10000))
 }
 count <- c(0, count)
 probabilities <- data.frame(x = count, p = probabilities)
